1e37b507989505c9ac9262fe7dd2bd0ed35a47d1
Anuj-Rathore24 <anujsingh2409@gmail.com>
2022-05-30 18:04:12 +0530

    initializing repo

--- app/src/main/java/com/example/myfirstapp/DataBaseHelper.java
+++ app/src/main/java/com/example/myfirstapp/DataBaseHelper.java
@@ -7,8 +7,30 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import java.io.Serializable;
+import java.util.List;
+
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+
+class selectData implements Serializable {
+    String Name,Email,MotherName,FatherName,AdmissionMode,PreferredBranch;
+    int Contact,Percentile;
+    selectData(String f,String e,int c,String mn,String fn,String pb,String am,int p){
+        Name=f;
+        PreferredBranch=pb;
+        Contact=c;
+        Percentile=p;
+
+        Email=e;
+        MotherName=mn;
+        FatherName=fn;
+        AdmissionMode=am;
+
+
+    }
+}
 public class DataBaseHelper extends SQLiteOpenHelper {
     public DataBaseHelper(@Nullable Context context) {
         super(context, "StudentDb", null, 1);
@@ -17,65 +39,64 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     //this is called the first time a database is accessed
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE IF NOT EXISTS StudentTable(FirstName VARCHAR(40),LastName VARCHAR(40),Age int,Email VARCHAR(40),Panel varchar(2));");
+        db.execSQL("CREATE TABLE IF NOT EXISTS CSE(Name VARCHAR(40),Email VARCHAR(40),Contact int,MotherName VARCHAR(40),FatherName varchar(40),PreferredBranch varchar(40),AdmissionMode varchar(40),Percentile int);");
+        db.execSQL("CREATE TABLE IF NOT EXISTS MECH(Name VARCHAR(40),Email VARCHAR(40),Contact int,MotherName VARCHAR(40),FatherName varchar(40),PreferredBranch varchar(40),AdmissionMode varchar(40),Percentile int);");
+        db.execSQL("CREATE TABLE IF NOT EXISTS ECE(Name VARCHAR(40),Email VARCHAR(40),Contact int,MotherName VARCHAR(40),FatherName varchar(40),PreferredBranch varchar(40),AdmissionMode varchar(40),Percentile int);");
+        db.execSQL("CREATE TABLE IF NOT EXISTS CIVIL(Name VARCHAR(40),Email VARCHAR(40),Contact int,MotherName VARCHAR(40),FatherName varchar(40),PreferredBranch varchar(40),AdmissionMode varchar(40),Percentile int);");
+        db.execSQL("CREATE TABLE IF NOT EXISTS LAW(Name VARCHAR(40),Email VARCHAR(40),Contact int,MotherName VARCHAR(40),FatherName varchar(40),PreferredBranch varchar(40),AdmissionMode varchar(40),Percentile int);");
+
     }
     //this is called if database version changes
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
-    public boolean insert_values(String firstname,String lastname,String age,String email,String panel){
+    public boolean insert_values(String TableName,String name,String email,int contact,String mothername,String fathername,String preferredname,String admissionmode,int percentile){
         ;
         SQLiteDatabase db=this.getWritableDatabase();
         ContentValues cv=new ContentValues();
-        cv.put("FirstName",firstname);
-        cv.put("LastName",lastname);
-        cv.put("Age",age);
+        cv.put("Name",name);
         cv.put("Email",email);
-        cv.put("Panel",panel);
+        cv.put("Contact",contact);
+        cv.put("MotherName",mothername);
+        cv.put("FatherName",fathername);
+        cv.put("PreferredBranch",preferredname);
+        cv.put("AdmissionMode",admissionmode);
+        cv.put("Percentile",percentile);
 
-        long insert=db.insert("StudentTable",null,cv);
+        long insert=db.insert(TableName,null,cv);
         if(insert==-1){
             return false;
         }
         return true;
     }
-    public String show_result(){
-        SQLiteDatabase db=this.getReadableDatabase();
-        String result="\nThe Database\n";
+    public List<selectData> show_result(String TableName){
 
-        Cursor resultSet=db.rawQuery("SELECT * FROM StudentTable;",null);
+        List<selectData> ourList=new ArrayList<selectData>();
+
+        SQLiteDatabase db=this.getReadableDatabase();
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS CSE(Name VARCHAR(40),Email VARCHAR(40),Contact int,MotherName VARCHAR(40),FatherName varchar(40),PreferredBranch varchar(40),AdmissionMode varchar(40),Percentile int);");
+        db.execSQL("CREATE TABLE IF NOT EXISTS MECH(Name VARCHAR(40),Email VARCHAR(40),Contact int,MotherName VARCHAR(40),FatherName varchar(40),PreferredBranch varchar(40),AdmissionMode varchar(40),Percentile int);");
+        db.execSQL("CREATE TABLE IF NOT EXISTS ECE(Name VARCHAR(40),Email VARCHAR(40),Contact int,MotherName VARCHAR(40),FatherName varchar(40),PreferredBranch varchar(40),AdmissionMode varchar(40),Percentile int);");
+        db.execSQL("CREATE TABLE IF NOT EXISTS CIVIL(Name VARCHAR(40),Email VARCHAR(40),Contact int,MotherName VARCHAR(40),FatherName varchar(40),PreferredBranch varchar(40),AdmissionMode varchar(40),Percentile int);");
+        db.execSQL("CREATE TABLE IF NOT EXISTS LAW(Name VARCHAR(40),Email VARCHAR(40),Contact int,MotherName VARCHAR(40),FatherName varchar(40),PreferredBranch varchar(40),AdmissionMode varchar(40),Percentile int);");
+
+
+
+        Cursor resultSet=db.rawQuery("SELECT * FROM "+"'"+TableName+"'"+" ;",null);
+
         resultSet.moveToFirst();
         while(!resultSet.isAfterLast()){
-            result+="\n"+resultSet.getString(0)+ " \t "+resultSet.getString(1)+ " \t "+resultSet.getString(2)+ " \t "+resultSet.getString(3)+ " \t "+resultSet.getString(4);
+
+            selectData data=new selectData(resultSet.getString(0),resultSet.getString(1),resultSet.getColumnIndex("Contact"),resultSet.getString(3),resultSet.getString(4),resultSet.getString(5),resultSet.getString(6),resultSet.getColumnIndex("Percentile"));
+            ourList.add(data);
             resultSet.moveToNext();
         }
         resultSet.close();
-        return result;
-    }
-    public boolean delete_records(String Email){
-
-        SQLiteDatabase db=this.getWritableDatabase();
-        int studentTable = db.delete("StudentTable", "Email=" + "'"+Email+"'", null);
-        if(studentTable==0){
-            return false;
-        }
-        return true;
+        return ourList;
     }
 
-    public boolean update_records(String firstname,String lastname,String age,String email,String panel){
-
-        SQLiteDatabase db=this.getWritableDatabase();
-        ContentValues cv=new ContentValues();
-        cv.put("FirstName",firstname);
-        cv.put("LastName",lastname);
-        cv.put("Age",age);
-        cv.put("Email",email);
-        cv.put("Panel",panel);
-        int studentTable = db.update("StudentTable",cv, "Email=" + "'"+email+"'", null);
-        if(studentTable==0){
-            return false;
-        }
-        return true;
-    }
 }
+
+
--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,29 +1,41 @@
 package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 
+import android.app.ActionBar;
 import android.content.Context;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
+import java.util.Iterator;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button add_button,delete_button,update_button,view_button;
+    private Button button;
     EditText firstname,lastname,age,email,panel;
 
     //setting up sqlite database
     DataBaseHelper dataBaseHelper=new DataBaseHelper(this);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,114 +45,88 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        add_button=findViewById(R.id.add_button);
-        view_button=findViewById(R.id.view_button);
-        delete_button=findViewById(R.id.delete_button);
-        update_button=findViewById(R.id.update_button);
 
 
 
 
         //adding functionality to our submit button
-        add_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("debugging", "working1 ");
-                firstname=findViewById(R.id.firstname_textbox);
-                lastname=findViewById(R.id.lastname_textbox);
-                age=findViewById(R.id.age_textbox);
-                email=findViewById(R.id.Email_textbox);
-                panel=findViewById(R.id.panel_textbox);
 
+                LinearLayout linearLayout =  (LinearLayout) findViewById(R.id.parentLayout);
+                linearLayout.removeAllViews();
+                Log.d("working","Hello There!");
 
                 try{
                     //calling method in our databaseHelper class for insertion process
+                    List<selectData> ourList;
+                    ourList=dataBaseHelper.show_result("CSE");
+                    Iterator<selectData> it=ourList.iterator();
+                    int i=0;
+                    while(it.hasNext()){
+                        i++;
+                        selectData row;
+                        row=it.next();
+                        LinearLayout container=new LinearLayout(MainActivity.this);
 
-                    boolean status=dataBaseHelper.insert_values(firstname.getText().toString(),lastname.getText().toString(),age.getText().toString(),email.getText().toString(),panel.getText().toString());
-                    Log.d("debugging", "Status ->"+status);
+                        LinearLayout.LayoutParams dimentions=new LinearLayout.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT,ActionBar.LayoutParams.MATCH_PARENT);
+                        dimentions.setMargins(20,20,20,20);
+                        container.setOrientation(LinearLayout.VERTICAL);
+
+                        container.setLayoutParams(dimentions);
+                        linearLayout.addView(container);
+
+                        TextView textName=new TextView(MainActivity.this);
+                        textName.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.RIGHT));
+
+                        container.addView(textName);
+                        textName.setText("Name :");
+                        textName.setTextColor(Color.parseColor("#000000"));
+
+                        TextView text=new TextView(MainActivity.this);
+                        ConstraintSet set = new ConstraintSet();
+                        container.addView(text);
+                        text.setText(row.Name);
+                        text.setTextColor(Color.parseColor("#000000"));
+
+
+                        Button btn=new Button(MainActivity.this);
+                        LinearLayout.LayoutParams btn_dimentions=new LinearLayout.LayoutParams(500, LinearLayout.LayoutParams.WRAP_CONTENT);
+                        btn.setLayoutParams(btn_dimentions);
+                        btn.setText("Click to View");
+                        container.addView(btn);
+
+                        btn.setOnClickListener(new View.OnClickListener() {
+
+                            @Override
+                            public void onClick(View v) {
+                                    Log.d("error_debug","\nworking\n");
+
+                                Intent intent=new Intent(v.getContext(),DIsplayData.class);
+                                intent.putExtra("OurObject",row);
+
+
+                                startActivity(intent);
+
+
+                            }
+
+                        });
+
+                    }
+                    Log.d("debugging", "Status ->");
 
                     //toast is the black text box generally used in our apps
-                    Toast.makeText(context, "New Record Added", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, "Selected!", Toast.LENGTH_SHORT).show();
 
                 }catch(Exception e){
                     System.out.println("The Error is ->"+e.toString());
                 }
 
             }
-        });
 
-        //Listener for View button
-        view_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final TextView database_contents=findViewById(R.id.database_info);
-
-                try{
-                   String result = dataBaseHelper.show_result();
-                    Log.i("Operations  ->", "Show result Status->"+result);
-                    database_contents.setText(result);
-
-                }catch(Exception e){
-                    System.out.println("The Error is ->"+e.toString());
-                }
-
-            }
-        });
-
-        //listener for Delete button
-        delete_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                email=findViewById(R.id.Email_textbox);
-                try{
-                    boolean status = dataBaseHelper.delete_records(email.getText().toString());
-                    if(status==false){
-                        Toast.makeText(context, "Record Not Found", Toast.LENGTH_SHORT).show();
-                    }else{
-                        Toast.makeText(context, "Record Deleted", Toast.LENGTH_SHORT).show();
-                    }
-                    Log.i("Operations", "Delete operation ->"+status);
-
-                }catch(Exception e){
-                    System.out.println("The Error is ->"+e.toString());
-                }
-
-            }
-        });
-
-
-        //listener for update button
-        update_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                firstname=findViewById(R.id.firstname_textbox);
-                lastname=findViewById(R.id.lastname_textbox);
-                age=findViewById(R.id.age_textbox);
-                email=findViewById(R.id.Email_textbox);
-                panel=findViewById(R.id.panel_textbox);
-
-
-                try{
-                    boolean status = dataBaseHelper.update_records(firstname.getText().toString(),lastname.getText().toString(),age.getText().toString(),email.getText().toString(),panel.getText().toString());
-                    if(status==false){
-                        Toast.makeText(context, "Record Not Found or Something went wrong", Toast.LENGTH_SHORT).show();
-                    }else{
-                        Toast.makeText(context, "Record Updated", Toast.LENGTH_SHORT).show();
-                    }
-                    Log.i("Operations", "Update operation ->"+status);
-
-                }catch(Exception e){
-                    System.out.println("The Error is ->"+e.toString());
-                }
-
-            }
-        });
 
 
     }
 
-
-}
 
 
 
